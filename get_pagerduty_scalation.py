638e20af7782c23fb743edebf4f0dcacb0794451
carolina-lima <lima.carolinadelima@gmail.com>
2022-12-08 16:21:06 -0300

    Fix file name on get_pagerduty

--- get_pagerduty_scalation.py
+++ get_pagerduty_scalation.py
@@ -5,7 +5,7 @@ from datetime import datetime
 def list_pagerduty_scalation():
     escalation = []
     f = '%Y-%m-%d %H:%M:%S'
-    with open('pagerduty_escalations_2022-07-01_2022-08-31 - pagerduty_escalations_2022-07-01_2022-08-31.csv', newline='') as csvfile:
+    with open('pagerduty_escalations.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             row['start'] = datetime.strftime((datetime.fromisoformat(row['start'])),f)
